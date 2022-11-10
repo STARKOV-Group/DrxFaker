0753975580c95c3493f948aac24e1dcfbaacc0c9
Vershinin <vershinin@starkovgrp.ru>
2022-11-10 14:29:56 +0500

    Реализация автоматического заполнения справочника "Тип справочника", во время инициализации [Rx]

--- starkov.Faker/starkov.Faker.ClientBase/DatabookType/DatabookTypeHandlers.cs
+++ starkov.Faker/starkov.Faker.ClientBase/DatabookType/DatabookTypeHandlers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
@@ -14,32 +14,6 @@ namespace starkov.Faker
     public override void Refresh(Sungero.Presentation.FormRefreshEventArgs e)
     {
       _obj.State.Properties.Name.IsEnabled = !string.IsNullOrEmpty(_obj.DatabookTypeGuid);
-    }
-
-    public virtual void DatabookTypeGuidValueInput(Sungero.Presentation.StringValueInputEventArgs e)
-    {
-      var guid = Guid.Empty;
-      if (!Guid.TryParse(e.NewValue, out guid))
-      {
-        e.AddError(starkov.Faker.DatabookTypes.Resources.ErrorInvalidGuid);
-        return;
-      }
-      
-      var type = TypeExtension.GetTypeByGuid(guid);
-      if (type == null)
-      {
-        e.AddError(starkov.Faker.DatabookTypes.Resources.ErrorGuidNotMatchAnyEntity);
-        return;
-      }
-      
-      var metadata = Sungero.Metadata.Services.MetadataSearcher.FindEntityMetadata(guid);
-      if (metadata.IsAbstract)
-      {
-        e.AddError(starkov.Faker.DatabookTypes.Resources.ErrorTypeIsAbstract);
-        return;
-      }
-      
-      _obj.Name = metadata.GetDisplayName();
     }
 
   }
